1f3636d4fcc8159bdfd146b17ddfffff96a197f6
James Mills <prologic@shortcircuit.net.au>
2020-04-08 11:39:00 +1000

    Update release to not commit a useless commit

--- tools/release.sh
+++ tools/release.sh
@@ -5,12 +5,15 @@ VERSION="$(git describe --abbrev=0 --tags)"
 VERSION=${VERSION:-'0.0.0'}
 
 # Get number parts
-MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
-MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
-PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+MAJOR="${VERSION%%.*}"
+VERSION="${VERSION#*.}"
+MINOR="${VERSION%%.*}"
+VERSION="${VERSION#*.}"
+PATCH="${VERSION%%.*}"
+VERSION="${VERSION#*.}"
 
 # Increase version
-PATCH=$((PATCH+1))
+PATCH=$((PATCH + 1))
 
 TAG="${1}"
 
@@ -21,12 +24,10 @@ fi
 echo "Releasing ${TAG} ..."
 
 git-chglog --next-tag="${TAG}" --output CHANGELOG.md
-git ci -a -m "Release ${TAG}"
-git push
 github-release release \
   -u prologic \
   -r ulinux \
-  -t "${TAG}"  \
+  -t "${TAG}" \
   -n "${TAG}" \
   -d "$(git-chglog --next-tag "${TAG}" "${TAG}" | tail -n+5)" \
   --draft
@@ -34,7 +35,7 @@ for asset in *.gz *.iso *.txt; do
   github-release upload \
     -u prologic \
     -r ulinux \
-    -t "${TAG}"  \
+    -t "${TAG}" \
     -f "${asset}" \
     -n "${asset}" \
     -l "${asset}"
