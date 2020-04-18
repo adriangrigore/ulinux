4515d10193c712a07d9ae406d22b2bce2d8c05dd
James Mills <prologic@shortcircuit.net.au>
2020-04-19 08:29:19 +1000

    Add extlinux as a core package

--- defs.sh
+++ defs.sh
@@ -9,7 +9,7 @@ SYSLINUX_VERSION=6.03
 RNGTOOLS_VERSION=5
 IPTABLES_VERSION=1.8.2
 
-CORE_PACKAGES="musl sinit make busybox dropbear"
+CORE_PACKAGES="musl sinit make busybox dropbear extlinux"
 CORE_PORTS="filesystem rc svc cloudinit net services ca-certificates tcc pkg box"
 
 [ -n "$WITH_SSL" ] && CORE_PACKAGES="$CORE_PACKAGES libressl"
