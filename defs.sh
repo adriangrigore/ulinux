86ecc23e929d3b9323bbc0f567fbd8a9071cdb45
James Mills <prologic@shortcircuit.net.au>
2020-04-19 12:05:45 +1000

    Fix filesystem port to pickup uLinux TAG and REV from the enviornment

--- defs.sh
+++ defs.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+# uLinux version/tag (passed in from the environment or defaulted to git)
+TAG="${TAG:-$(git describe --abbrev=0 --tags || echo "v0.0.0")}"
+REV="${REV:-$(git rev-parse --short HEAD || echo "0000000")}"
+
 KERNEL_VERSION=5.6.3
 SYSLINUX_VERSION=6.03
 
@@ -12,6 +16,9 @@ CORE_PORTS="filesystem rc svc cloudinit net services ca-certificates tcc pkg box
 
 [ -n "$WITH_SSL" ] && CORE_PACKAGES="$CORE_PACKAGES libressl"
 [ -n "$WITH_IPTABLES" ] && CORE_PACKAGES="$CORE_PACKAGES iptables"
+
+export TAG
+export REV
 
 export KERNEL_VERSION
 export SYSLINUX_VERSION
