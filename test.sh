32a2c8567a4ffe6dfbae142c8bad533aef8b17a4
James Mills <prologic@shortcircuit.net.au>
2020-04-20 10:23:24 +1000

    Fix test.sh to optionally include the clouddrive iso

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ fi
 QEMU_OPTS=
 
 [ -n "$HEADLESS" ] && QEMU_OPTS="$QEMU_OPTS -nographic"
+[ -n "$CLOUDDRIVE" ] && QEMU_OPTS="$QEMU_OPTS -drive file=clouddrive.iso,index=1,media=cdrom"
 
 QEMU_OPTS="$QEMU_OPTS
 -accel $accel,thread=multi
@@ -17,9 +18,8 @@ QEMU_OPTS="$QEMU_OPTS
 -rtc base=utc,clock=host
 -boot order=cd,menu=off
 -drive file=ulinux.iso,index=0,media=cdrom
--drive file=clouddrive.iso,index=1,media=cdrom
 -drive file=ulinux.img,media=disk,cache=writethrough
 -device virtio-rng-pci
 -net nic -net user,hostfwd=tcp::2222-:22"
 
-exec qemu-system-x86_64 $QEMU_OPTS 2> /dev/null
+exec qemu-system-x86_64 $QEMU_OPTS
